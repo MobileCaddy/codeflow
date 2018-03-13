867c50b50bbef25c4f81a7e900e6bc1454e2cb74
Todd Halfpenny <todd@mobilecaddy.net>
2018-03-13 15:47:10 +0000

    Adding setup script to move bits around

--- bin/mobilecaddy-app-scripts.js
+++ bin/mobilecaddy-app-scripts.js
@@ -1,3 +1,53 @@
 #!/usr/bin/env node
+var fs = require('fs');
 
-console.log("Hi");
+if (process.argv.length > 2) {
+
+  switch (process.argv[2]) {
+  	case 'setup':
+  		copySetupFiles();
+  		break;
+  	default:
+  		console.error('Unknown task: ' + process.argv[2]);
+  }
+
+} else {
+  console.log('Missing MobileCaddy app script task name');
+}
+
+function copySetupFiles(){
+	const destJsDir = 'src/assets/js';
+
+	if (!fs.existsSync(destJsDir)){
+	    fs.mkdirSync(destJsDir);
+	}
+
+	const destWWWDir = 'www';
+
+	if (!fs.existsSync(destWWWDir)){
+	    fs.mkdirSync(destWWWDir);
+	}
+	const sourceCodeFlowDir = 'node_modules/mobilecaddy-codeflow/js/';
+	const sourceForceJSDir = 'node_modules/forcejs/';
+
+	fs.copyFile(sourceCodeFlowDir + 'MockCordova.js', destJsDir + '/MockCordova.js', (err) => {
+    if (err) throw err;
+    console.log('Copying setup files...');
+	});
+	fs.copyFile(sourceCodeFlowDir + 'mockVFRemote.js', destJsDir + '/mockVFRemote.js', (err) => {
+    if (err) throw err;
+    console.log('...');
+	});
+	fs.copyFile(sourceCodeFlowDir + 'codeflow-emulator.js', destJsDir + '/codeflow-emulator.js', (err) => {
+    if (err) throw err;
+    console.log('...');
+	});
+	fs.copyFile(sourceForceJSDir + 'force.js', destJsDir + '/force.js', (err) => {
+    if (err) throw err;
+    console.log('...');
+	});
+	fs.copyFile('src/oauthcallback.html', destWWWDir + '/oauthcallback.html', (err) => {
+    if (err) throw err;
+    console.log('...');
+	});
+}
