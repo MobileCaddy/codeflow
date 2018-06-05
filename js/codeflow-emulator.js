015cc3609f8cbbf63afa219775fa296edb401b9f
Todd Halfpenny <todd@mobilecaddy.net>
2018-06-05 14:06:53 +0100

    Adding www/mc-params usage to codeflow-emulator

--- js/codeflow-emulator.js
+++ js/codeflow-emulator.js
@@ -7,6 +7,7 @@
 
 window.LOCAL_DEV = true;
 
+setScrub();
 setLocalDev();
 importScripts().then(function(result){
 	// Cool
@@ -33,19 +34,99 @@ function importScripts() {
 	});
 }
 
+function setScrub() {
+  let params = new URL(document.location).searchParams;
+  let qScrub = params.get('scrub');
+  switch (qScrub) {
+    case 'full':
+      localStorage.clear();
+      break;
+    case 'true':
+      let forceOAuth = localStorage.getItem('forceOAuth');
+      localStorage.clear();
+      localStorage.setItem('forceOAuth', forceOAuth);
+      break;
+    case 'false':
+      break;
+    default:
+      // If we get here then we have not got our scrub param
+      let myUri = 'mc-params.json';
 
-function setLocalDev(){
-	// Setup whether to use Mock, or to init forcejs
-	window.USE_FORCETK = true;
-	console.log("window.LOCAL_DEV", window.LOCAL_DEV);
+      let httpRequest = new XMLHttpRequest();
+      httpRequest.open('GET', myUri);
+      httpRequest.send();
 
-	var queryString= [];
-	var hash;
-	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-	for(var i = 0; i < hashes.length; i++)
-	{
-	    hash = hashes[i].split('=');
-	    if (hash[0] == 'local') window.USE_FORCETK = false;
-	}
+      httpRequest.onreadystatechange = function() {
+        // Process the server response here.
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status === 200) {
+            console.log(
+              'Read from file ' + myUri + ' OK',
+              httpRequest.responseText
+            );
+            let scrub = JSON.parse(httpRequest.responseText).scrub;
+            switch (scrub) {
+              case 'full':
+                localStorage.clear();
+                break;
+              case true: {
+                let forceOAuth = localStorage.getItem('forceOAuth');
+                localStorage.clear();
+                localStorage.setItem('forceOAuth', forceOAuth);
+                break;
+              }
+            }
+          } else {
+            console.error(
+              'Error reading from file' + myUri + ' -> ' + JSON.stringify(e)
+            );
+          }
+        }
+      };
+  }
+}
+
+
+function setLocalDev() {
+  // Setup whether to use Mock, or to init forcejs
+  window.USE_FORCETK = true;
+  console.log('window.LOCAL_DEV', window.LOCAL_DEV);
+
+  var params = new URL(document.location).searchParams;
+  var useMock = params.get('local');
+  console.log('params.has', params.has('local'));
+  console.log('useMock', useMock);
+  if (params.has('local') && (useMock === '' || useMock == 'true')) {
+    window.USE_FORCETK = false;
+  }
+  console.log('window.USE_FORCETK', window.USE_FORCETK);
+
+  if (window.USE_FORCETK === true) {
+    console.log('useMock', useMock);
+    // check the mc-params.json
+    let myUri = 'mc-params.json';
+
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', myUri);
+    httpRequest.send();
+
+    httpRequest.onreadystatechange = function() {
+      // Process the server response here.
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          console.log(
+            'Read from file ' + myUri + ' OK',
+            httpRequest.responseText
+          );
+          let local = JSON.parse(httpRequest.responseText).local;
+          if (local) window.USE_FORCETK = false;
+        } else {
+          console.error(
+            'Error reading from file' + myUri + ' -> ' + JSON.stringify(e)
+          );
+        }
+      }
+    };
+  }
 }
 
